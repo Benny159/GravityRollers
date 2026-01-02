3e8d64bf5ee86a3af5cbfb9b2c8804de6725c630
Benjamin <bejamin.159@web.de>
2026-01-02 13:43:45 +0100

    [IMP]Logic for Checkpoints

--- Source/GravityRollers/Private/Checkpoint.cpp
+++ Source/GravityRollers/Private/Checkpoint.cpp
@@ -1,61 +1,62 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Checkpoint.h"
 #include "Marble.h"
-#include "Components/BoxComponent.h"
-#include "Components/StaticMeshComponent.h"
+#include "MarbleGameMode.h"
+#include "Kismet/GameplayStatics.h"
 
-// Sets default values
 ACheckpoint::ACheckpoint()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = false;
-	
-	USceneComponent* SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
-	RootComponent = SceneRoot;
-	
-	TriggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerBox"));
-	TriggerBox->SetupAttachment(RootComponent);
-	TriggerBox->SetBoxExtent(FVector(200.f, 200.f, 100.f));
-	TriggerBox->SetCollisionProfileName(TEXT("Trigger"));
-	CheckpointIndex = -1;
+    PrimaryActorTick.bCanEverTick = false;
+    
+    USceneComponent* SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
+    RootComponent = SceneRoot;
+    
+    //CheckpointMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CheckpointMesh"));
+    //CheckpointMesh->SetupAttachment(RootComponent);
+    //CheckpointMesh->SetCollisionProfileName(TEXT("NoCollision")); 
+    
+    TriggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerBox"));
+    TriggerBox->SetupAttachment(RootComponent);
+    TriggerBox->SetBoxExtent(FVector(200.f, 50.f, 100.f));
+    TriggerBox->SetCollisionProfileName(TEXT("Trigger"));
+
+    CheckpointIndex = -1;
 }
 
-// Called when the game starts or when spawned
 void ACheckpoint::BeginPlay()
 {
-	Super::BeginPlay();
-	if (TriggerBox)
-	{
-		TriggerBox->OnComponentBeginOverlap.AddDynamic(this, &ACheckpoint::OnOverlapBegin);
-	}
-	
-}
-
-void ACheckpoint::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-{
-	if (OtherActor && (OtherActor != this)) 
-	{
-		AMarble* Marble = Cast<AMarble>(OtherActor);
-
-		if (Marble)
-		{
-			float CurrentServerTime = GetWorld()->GetTimeSeconds();
-			// Marble->PassCheckpoint(CheckpointIndex, CurrentServerTime);
-		}
-	}
+    Super::BeginPlay();
+    
+    if (TriggerBox)
+    {
+        TriggerBox->OnComponentBeginOverlap.AddDynamic(this, &ACheckpoint::OnOverlapBegin);
+    }
 }
 
 void ACheckpoint::SetCheckpointIndex(int32 NewIndex)
 {
-	CheckpointIndex = NewIndex;
+    CheckpointIndex = NewIndex;
 }
 
-// Called every frame
-void ACheckpoint::Tick(float DeltaTime)
+void ACheckpoint::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	Super::Tick(DeltaTime);
+    AMarble* Marble = Cast<AMarble>(OtherActor);
 
+    if (Marble)
+    {
+        float CurrentRaceTime = 0.0f;
+        AMarbleGameMode* GM = Cast<AMarbleGameMode>(UGameplayStatics::GetGameMode(this));
+        
+        if (GM) 
+        {
+            CurrentRaceTime = GM->GetCurrentRaceTime();
+        }
+        else 
+        {
+            CurrentRaceTime = GetWorld()->GetTimeSeconds();
+        }
+        
+        float CurrentSpeed = Marble->GetVelocity().Size();
+        
+        Marble->PassCheckpoint(CheckpointIndex, CurrentRaceTime, CurrentSpeed);
+    }
 }
-
--- Source/GravityRollers/Public/Checkpoint.h
+++ Source/GravityRollers/Public/Checkpoint.h
@@ -1,10 +1,8 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Components/BoxComponent.h"
-#include "Components/TextRenderComponent.h" // Optional: Für Nummern über dem Checkpoint
 #include "Checkpoint.generated.h"
 
 UCLASS()
@@ -19,27 +17,17 @@ protected:
 	virtual void BeginPlay() override;
 
 public:
-	// --- KOMPONENTEN ---
-
-	// Die unsichtbare Kollisionsbox (Der Auslöser)
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	UBoxComponent* TriggerBox;
-
-	// Das visuelle Modell (z.B. Torbogen, Fähnchen)
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-	UStaticMeshComponent* CheckpointMesh;
-
-	// --- LOGIK ---
-
-	// Welcher Checkpoint ist das? (0, 1, 2...)
-	// Wird von der RaceTrack automatisch gesetzt, kann aber im Editor geprüft werden
+	
+	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	//UStaticMeshComponent* CheckpointMesh;
+	
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Race Logic")
 	int32 CheckpointIndex;
-
-	// Hilfsfunktion zum Setzen des Index (durch RaceTrack)
+	
 	void SetCheckpointIndex(int32 NewIndex);
-
-	// Das Event, wenn etwas durchfliegt
+	
 	UFUNCTION()
 	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 };
